059b6b8681fa59e9b58dd0444bd1c0a860c85e65
graceyang2026 <13716726157@139.com>
2026-05-09 10:56:37 +0800

    AI-Qoder-delete manual code

--- src/main/java/com/yangwei/ai/ai_vendor_management_system/repository/SupplierRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yangwei.ai.ai_vendor_management_system.repository;
-
-
-import com.yangwei.ai.ai_vendor_management_system.entity.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Long> {
-}
